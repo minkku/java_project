cad7e23d520b8cadbe96ef4c114e326000a0e963
minkku <0106995@naver.com>
2023-11-21 17:27:38 +0900

    Add LoginController

---
+++ Project/src/main/java/org/sara/domain/UserVO.java
@@ -0,0 +1,84 @@
+package org.sara.domain;
+
+public class UserVO {
+	private int user_id;
+	private String name;
+	private String pw;
+	private String address;
+	private String gender;
+	private String email;
+	private String mobile;
+	private int point;
+	
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	@Override
+	public String toString() {
+		return "UserVO [user_id=" + user_id + ", name=" + name + ", pw=" + pw + ", address=" + address + ", gender="
+				+ gender + ", email=" + email + ", mobile=" + mobile + ", point=" + point + "]";
+	}
+}
+
+
