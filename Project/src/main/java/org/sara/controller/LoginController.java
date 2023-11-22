d7d8e766f89a9d93fd5f538d00101165be46bb29
minkku <0106995@naver.com>
2023-11-22 18:05:02 +0900

    wip

--- Project/src/main/java/org/sara/controller/LoginController.java
+++ Project/src/main/java/org/sara/controller/LoginController.java
@@ -1,5 +1,24 @@
 package org.sara.controller;
 
-public class LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/")
+@Log4j
+@AllArgsConstructor
+public class LoginController {
+	@GetMapping("/login")
+	public String loginPage() {
+		return "login";
+	}
+	
+	@GetMapping("/join")
+	public String joinPage() {
+		return "join";
+	}
 }
