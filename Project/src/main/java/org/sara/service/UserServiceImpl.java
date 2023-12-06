4c429c2872a2c45857a1d41bbe339781198990b4
minkku <0106995@naver.com>
2023-12-06 23:20:08 +0900

    working on bcrypt

--- Project/src/main/java/org/sara/service/UserServiceImpl.java
+++ Project/src/main/java/org/sara/service/UserServiceImpl.java
@@ -2,6 +2,9 @@ package org.sara.service;
 
 import org.sara.domain.UserVO;
 import org.sara.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -13,11 +16,29 @@ import lombok.extern.log4j.Log4j;
 public class UserServiceImpl implements UserService {
 	private UserMapper mapper;
 
+	// 유저가 회원가입시 비밀번호를 암호화 시켜주기 위한 설정
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
 	 @Override
-	 public void reg(UserVO user) throws Exception {
-		 log.info("Registering user: " + user);
-	    mapper.reg(user);
+	 public void reg(UserVO user) throws DuplicateKeyException, InvalidPasswordException, Exception {
+			try {
+			// 사용자가 입력한 원본 비밀번호를 Bcrypt로 해싱하여 저장
+			if(!isValidPassword(user.getPw())) {
+				throw new InvalidPasswordsException("Invalid password");
+			}
+			String hashedPassword = passwordEncoder.encode(user.getPw());
+			user.setPw(hashedPassword);;
+			
+			log.info("Registering user: " + user);
+			mapper.reg(user);
+		} catch (DuplicateKeyException e) {
+			throw new DuplicateKeyException("Email address already exists.", e);
+		} catch (Exception e) {
+			throw new RegistrationException("Error during user registration.", e);
+		}
 	 }
+	
 	 
 	 @Override
 	 public int emailCheck(String email) throws Exception {
